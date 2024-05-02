56ae0ad383e373fda1753bb4f78430111ae221fd
Trix <157342883+TrixSec@users.noreply.github.com>
2024-05-03 02:10:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
 # config.py
 
 # Telegram bot token
-TOKEN = "7079291086:AAFvFPcUWt-3AwlbCSYvsJbtRtXRgFB4G_w"
-
-# Other bot token for notification
-OTHER_BOT_TOKEN = "6988435017:AAHSD-CMNh-39xVa_rYhR0ic8BbefWzD9jQ"
-
-# Other bot chat ID for notification
-OTHER_BOT_CHAT_ID = "5542562018"
+TOKEN = "Paste Bot Token"
