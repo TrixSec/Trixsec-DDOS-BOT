7fb9ec16fa128c21803c0a143842105e77ac882f
Trix <157342883+TrixSec@users.noreply.github.com>
2024-05-03 02:05:40 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Telegram bot token
+TOKEN = "7079291086:AAFvFPcUWt-3AwlbCSYvsJbtRtXRgFB4G_w"
+
+# Other bot token for notification
+OTHER_BOT_TOKEN = "6988435017:AAHSD-CMNh-39xVa_rYhR0ic8BbefWzD9jQ"
+
+# Other bot chat ID for notification
+OTHER_BOT_CHAT_ID = "5542562018"
